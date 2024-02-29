4f1f558c333a48ddc9aa9402161cb094832384fc
ztucker4 <zoetucker@library.ucla.edu>
2024-02-29 12:54:21 -0800

    SYS-1513: Finalize bookplate script (#29)
    * initial script changes to use 966
    * fix 966 subfield inserts
    * update duplicate matching to check 966 a-c
    * finish 966 bookplate logic, and add tests
    * clean up sample data csv
    * fix data typos and remove unneeded bib updates
    * fix logging and test setup
    * support updating SPAC names for matching codes, and add test
    * remove unused spac_name refs from is_new_966()

--- add_bib_ebookplates.py
+++ add_bib_ebookplates.py
@@ -1,14 +1,18 @@
 import csv
 import copy
 import argparse
+import logging
 from alma_api_keys import API_KEYS
 from alma_api_client import AlmaAPIClient
 from alma_analytics_client import AlmaAnalyticsClient
 from alma_marc import get_pymarc_record_from_bib, prepare_bib_for_update
-from pymarc import Field, Record
+from pymarc import Field, Record, Subfield
+
+logging.basicConfig(filename="add_bib_ebookplates.log", level=logging.DEBUG)
 
 
 def get_fund_code_report(analytics_api_key: str) -> list:
+    """Get the report of MMS IDs and fund codes from Alma Analytics."""
     # analytics only available in prod environment
     aac = AlmaAnalyticsClient(analytics_api_key)
     report_path = (
@@ -20,24 +24,35 @@ def get_fund_code_report(analytics_api_key: str) -> list:
     return report
 
 
-def get_ebookplates(report: list, input_file: str) -> list:
+def get_report_ebookplates(report: list, input_file: str) -> list:
+    """Add SPAC ebookplate info to each item in the report."""
     # copy SPAC mappings into list of dicts for looping over
     spac_mappings = []
     with open(input_file, newline="", encoding="utf-8-sig") as csv_file:
         reader = csv.DictReader(csv_file)
         for line in reader:
-            spac_mappings.append(line)
+            # remove leading/trailing whitespace from all values
+            line = {k: v.strip() for k, v in line.items()}
+            # check the FUND column for commas, indicating multiple funds
+            if "," in line["FUND"]:
+                # split on commas and add a new line for each fund
+                funds = line["FUND"].split(", ")
+                for fund in funds:
+                    current_line = copy.deepcopy(line)
+                    current_line["FUND"] = fund
+                    spac_mappings.append(current_line)
+            else:
+                spac_mappings.append(line)
 
     # create new list of dicts for items to avoid changing as we iterate over report
     new_report = []
     for item in report:
         for line in spac_mappings:
-            if line["Alma Fund Code"] == item["Fund Ledger Code"]:
+            if line["FUND"] == item["Fund Code"]:
                 current_item = copy.deepcopy(item)
-                current_item["spac_code"] = line["SPAC_CODE"]
-                current_item["spac_name"] = line["SPAC_NAME"]
-                current_item["spac_url"] = line["E-bookplate link"]
-                current_item["spac_image"] = line["Bookplate Image Link"]
+                current_item["spac_code"] = line["SPAC"]
+                current_item["spac_name"] = line["NAME"]
+                current_item["spac_url"] = line["URL"]
                 new_report.append(current_item)
 
     # sanity check - same number of items before and after SPAC mapping?
@@ -49,72 +64,66 @@ def get_ebookplates(report: list, input_file: str) -> list:
     return new_report
 
 
-def insert_ebookplates(alma_api_key: str, report: list) -> tuple[int, int, int]:
-    client = AlmaAPIClient(alma_api_key)
-    # keep track of # of bibs updated for later reporting
-    total_updated = 0
-    total_skipped = 0
-    total_errored = 0
-
-    for item in report:
-        mms_id = item["MMS Id"]
-        spac_name = item["spac_name"]
-        # spac_url will be an empty string for some SPACs - this is ok!
-        spac_url = item["spac_url"]
-        # placeholder text for now - this will eventually vary with each SPAC
-        spac_image = item["spac_image"]
-
-        # get bib from Alma
-        alma_bib = client.get_bib(mms_id).get("content")
-        # make sure we got a valid bib
-        if b"is not valid" in alma_bib:
-            total_errored += 1
-            print(
-                f"Got an error finding bib record for MMS ID {mms_id}. Skipping this record."
-            )
-            continue
-
-        # convert to Pymarc to handle fields and subfields
-        pymarc_record = get_pymarc_record_from_bib(alma_bib)
-
-        # first check for existing 967, matching on $a
-        if is_not_duplicate_967(pymarc_record, spac_name):
-            # always have spac name in $a
-            subfields = ["a", spac_name]
-            # add $b and $c if they exist
-            if spac_url:
-                subfields.extend(["b", spac_url])
-            if spac_image:
-                subfields.extend(["c", spac_image])
-            pymarc_record.add_field(
-                Field(
-                    tag="967",
-                    # alma_marc.prepare_bib_for_update needs indicators explicitly set
-                    indicators=[" ", " "],
-                    subfields=subfields,
-                )
-            )
-            # repackage Alma bib and send update
-            new_alma_bib = prepare_bib_for_update(alma_bib, pymarc_record)
-            client.update_bib(mms_id, new_alma_bib)
-            print(f"Added SPAC to bib. MMS ID: {mms_id}, SPAC Name: {spac_name}")
-            total_updated += 1
-
-        # print extra info if a duplicate 967 is found
-        else:
-            print(
-                f"Skipped bib with existing 967 SPAC. MMS ID: {mms_id}, SPAC Name: {spac_name}"
-            )
-            total_skipped += 1
-
-    return total_updated, total_skipped, total_errored
-
-
-def is_not_duplicate_967(old_record: Record, spac_name: str) -> bool:
-    for field_967 in old_record.get_fields("967"):
-        if spac_name in field_967.get_subfields("a"):
+def is_new_966(old_record: Record, spac_code: str) -> bool:
+    """Check all 966 fields in a record to see if a new 966 field is needed."""
+    for field_966 in old_record.get_fields("966"):
+        # match only subfield a
+        if spac_code in field_966.get_subfields("a"):
             return False
     return True
+
+
+def needs_bookplate_update(
+    old_field: Field, spac_code: str, spac_name: str, spac_url: str
+) -> bool:
+    """Check if a 966 field matches the SPAC code, but needs an update to URL or name."""
+    # First, match on subfield a. If no match, this field doesn't need updating.
+    # get_subfields returns a list, we expect only one $a,b,c per 966 field
+    if spac_code != old_field.get_subfields("a")[0]:
+        return False
+    # If the new URL is an empty string, check if $c exists. If it does, update is needed.
+    elif (not spac_url) and (old_field.get_subfields("c")):
+        return True
+    # If the new URL is not empty, check if it matches the existing $c. If not, update is needed.
+    elif spac_url:
+        # if we have a URL but no $c subfield, update is needed
+        if not old_field.get_subfields("c"):
+            return True
+        # otherwise, compare the URL in the 966 field to the new URL
+        if spac_url != old_field.get_subfields("c")[0]:
+            return True
+    # Now check if the bookplate text needs updating
+    if spac_name != old_field.get_subfields("b")[0]:
+        return True
+
+
+def add_new_966(record: Record, spac_code: str, spac_name: str, spac_url: str) -> None:
+    """Add a new 966 field to a pymarc record, with SPAC and bookplate data."""
+    subfields = []
+    subfields.append(Subfield(code="a", value=spac_code))
+    subfields.append(Subfield(code="b", value=spac_name))
+    subfields.append(Subfield(code="9", value="LOCAL"))
+    if spac_url:
+        subfields.append(Subfield(code="c", value=spac_url))
+    record.add_field(
+        Field(
+            tag="966",
+            indicators=[" ", " "],
+            subfields=subfields,
+        )
+    )
+
+
+def update_existing_966(field_966: Field, spac_name: str, spac_url: str) -> None:
+    """Update the URL and bookplate text in an existing 966 field."""
+    # update $b for bookplate text
+    field_966.delete_subfield("b")
+    field_966.add_subfield("b", spac_name)
+    # update $c for URL
+    field_966.delete_subfield("c")
+    # if spac_url is an empty string, don't add $c back in
+    if spac_url:
+        field_966.add_subfield("c", spac_url)
 
 
 def main():
@@ -122,21 +131,28 @@ def main():
     parser.add_argument(
         "spac_mappings_file", help="Path to the SPAC mappings .csv file"
     )
-    parser.add_argument("environment", help="Alma environment (sandbox or production)")
+    parser.add_argument(
+        "environment",
+        help="Alma environment (sandbox or production), or 'debug' for a small test set.",
+    )
     args = parser.parse_args()
 
-    if args.environment == "sandbox":
+    if args.environment == "debug":
         # test data for sandbox environment
+        # these MMS IDs are real, but fund codes are fake to align with test SPAC mappings file
         report_data = [
-            {
-                "MMS Id": "9996854839106533",
-                "Fund Ledger Code": "2SC010",
-                "Transaction Date": "2022-04-15T00:00:00",
-                "Transaction Item Type": "EXPENDITURE",
-                "Invoice-Number": "9300014049",
-            }
+            # case 1: SPAC1, with URL
+            {"MMS Id": "9911656853606533", "Fund Code": "FUND2A"},
+            # case 2: SPAC3, no URL
+            {"MMS Id": "9990572683606533", "Fund Code": "FUND3"},
         ]
         alma_api_key = API_KEYS["SANDBOX"]
+
+    elif args.environment == "sandbox":
+        # use production analytics key for sandbox environment, since sandbox doesn't have analytics
+        analytics_api_key = API_KEYS["DIIT_ANALYTICS"]
+        alma_api_key = API_KEYS["SANDBOX"]
+        report_data = get_fund_code_report(analytics_api_key)
 
     elif args.environment == "production":
         analytics_api_key = API_KEYS["DIIT_ANALYTICS"]
@@ -146,17 +162,73 @@ def main():
     print(f"Beginning processing {len(report_data)} bib e-bookplates")
     print()
 
-    report_with_ebookplates = get_ebookplates(report_data, args.spac_mappings_file)
-    total_updated, total_skipped, total_errored = insert_ebookplates(
-        alma_api_key, report_with_ebookplates
+    report_with_ebookplates = get_report_ebookplates(
+        report_data, args.spac_mappings_file
     )
+
+    client = AlmaAPIClient(alma_api_key)
+
+    # initialize counters
+    total_bibs_updated = 0
+    total_bibs_skipped = 0
+    total_bibs_errored = 0
+
+    for item in report_with_ebookplates:
+        mms_id = item["MMS Id"]
+        spac_code = item["spac_code"]
+        spac_name = item["spac_name"]
+        spac_url = item["spac_url"]
+        bib_was_updated = False
+
+        # get bib from Alma
+        alma_bib = client.get_bib(mms_id).get("content")
+        # check for error in bib response, usually due to invalid MMS ID
+        if b"errorsExist" in alma_bib:
+            logging.info(
+                f"Got an error finding bib record for MMS ID {mms_id}. Skipping this record."
+            )
+            total_bibs_errored += 1
+            continue
+
+        # convert to Pymarc to handle fields and subfields
+        pymarc_record = get_pymarc_record_from_bib(alma_bib)
+
+        if is_new_966(pymarc_record, spac_code):
+            add_new_966(pymarc_record, spac_code, spac_name, spac_url)
+            logging.debug(
+                f"Added new bookplate to bib. MMS ID: {mms_id}, SPAC Name: {spac_name}"
+            )
+            bib_was_updated = True
+        else:
+            for field_966 in pymarc_record.get_fields("966"):
+                if needs_bookplate_update(field_966, spac_code, spac_name, spac_url):
+                    update_existing_966(field_966, spac_name, spac_url)
+                    logging.debug(
+                        f"Updated bookplate. MMS ID: {mms_id}, SPAC Name: {spac_name}",
+                    )
+                    bib_was_updated = True
+
+        if bib_was_updated:
+            new_alma_bib = prepare_bib_for_update(alma_bib, pymarc_record)
+            client.update_bib(mms_id, new_alma_bib)
+            total_bibs_updated += 1
+        else:
+            total_bibs_skipped += 1
+            logging.debug(f"Skipping MMS ID {mms_id}. No 966 updates needed.")
+
+        # every 5% of records, log progress
+        total_bibs_processed = (
+            total_bibs_updated + total_bibs_skipped + total_bibs_errored
+        )
+        if total_bibs_processed % (len(report_with_ebookplates) / 20) == 0:
+            logging.info(f"Processed {total_bibs_processed} bibs.")
 
     print()
     print(
         "Finished adding ebookplates. ",
-        f"{total_updated} bibs updated. ",
-        f"{total_skipped} bibs skipped due to duplicate 967s.",
-        f"{total_errored} bibs skipped due to errors.",
+        f"{total_bibs_updated} bibs updated. ",
+        f"{total_bibs_skipped} bibs skipped with no 966 updates needed. ",
+        f"{total_bibs_errored} bibs skipped due to errors.",
     )
 
 
---
+++ tests/test_add_bib_ebookplates.py
@@ -0,0 +1,223 @@
+import unittest
+from pymarc import Field, Record, Subfield
+from add_bib_ebookplates import (
+    get_report_ebookplates,
+    is_new_966,
+    needs_bookplate_update,
+    add_new_966,
+    update_existing_966,
+)
+
+
+class TestAddBibEbookplates(unittest.TestCase):
+
+    def test_get_report_ebookplates(self):
+        sample_mapping_file = "tests/data/sample_SPAC_mappings.csv"
+        sample_report_data = [
+            {"MMS Id": "MMS1", "Fund Code": "FUND1"},
+        ]
+        report_with_ebookplates = get_report_ebookplates(
+            sample_report_data, sample_mapping_file
+        )
+        self.assertEqual(report_with_ebookplates[0]["spac_code"], "SPAC1")
+        self.assertEqual(report_with_ebookplates[0]["spac_name"], "Bookplate Label #1")
+        self.assertEqual(report_with_ebookplates[0]["spac_url"], "https://example.com")
+
+    def test_get_report_ebookplates_no_url(self):
+        sample_mapping_file = "tests/data/sample_SPAC_mappings.csv"
+        sample_report_data = [
+            {"MMS Id": "MMS1", "Fund Code": "FUND3"},
+        ]
+        report_with_ebookplates = get_report_ebookplates(
+            sample_report_data, sample_mapping_file
+        )
+        self.assertEqual(report_with_ebookplates[0]["spac_code"], "SPAC3")
+        self.assertEqual(report_with_ebookplates[0]["spac_name"], "Bookplate Label #3")
+        self.assertEqual(report_with_ebookplates[0]["spac_url"], "")
+
+    def test_get_report_ebookplates_multiple_funds_single_spac(self):
+        sample_mapping_file = "tests/data/sample_SPAC_mappings.csv"
+        sample_report_data = [
+            {"MMS Id": "MMS1", "Fund Code": "FUND2A"},
+            {"MMS Id": "MMS2", "Fund Code": "FUND2B"},
+        ]
+        report_with_ebookplates = get_report_ebookplates(
+            sample_report_data, sample_mapping_file
+        )
+        # in mapping file, FUND2A and FUND2B both map to SPAC2
+        # this also tests that comma separated fund codes are handled correctly
+        self.assertEqual(report_with_ebookplates[0]["spac_code"], "SPAC2")
+        self.assertEqual(report_with_ebookplates[0]["spac_name"], "Bookplate Label #2")
+        self.assertEqual(
+            report_with_ebookplates[0]["spac_url"], "https://another-example.com"
+        )
+        self.assertEqual(report_with_ebookplates[1]["spac_code"], "SPAC2")
+        self.assertEqual(report_with_ebookplates[1]["spac_name"], "Bookplate Label #2")
+        self.assertEqual(
+            report_with_ebookplates[1]["spac_url"], "https://another-example.com"
+        )
+
+    def test_is_new_966_original_empty(self):
+        record = Record()
+        spac_code = "SPAC"
+        self.assertTrue(is_new_966(record, spac_code))
+
+    def test_is_new_966_exact_match(self):
+        record = Record()
+        spac_code = "SPAC"
+        record.add_field(
+            Field(
+                tag="966",
+                indicators=[" ", " "],
+                subfields=[
+                    Subfield(code="a", value=spac_code),
+                ],
+            )
+        )
+        self.assertFalse(is_new_966(record, spac_code))
+
+    def test_needs_bookplate_update_no_matching_ab(self):
+        old_field = Field(
+            tag="966",
+            indicators=[" ", " "],
+            subfields=[
+                Subfield(code="a", value="SPAC"),
+                Subfield(code="b", value="SPAC Name"),
+                Subfield(code="c", value="https://example.com"),
+            ],
+        )
+        spac_code = "NOTSPAC"
+        spac_name = "Different SPAC Name"
+        spac_url = "https://example.com"
+
+        # This is a new 966 field, so it doesn't need updating
+        self.assertFalse(
+            needs_bookplate_update(old_field, spac_code, spac_name, spac_url)
+        )
+
+    def test_needs_bookplate_update_no_original_c(self):
+        old_field = Field(
+            tag="966",
+            indicators=[" ", " "],
+            subfields=[
+                Subfield(code="a", value="SPAC"),
+                Subfield(code="b", value="SPAC Name"),
+            ],
+        )
+        spac_code = "SPAC"
+        spac_name = "SPAC Name"
+        spac_url = "https://example.com"
+
+        # We will need to update the URL, since the original 966 field has no $c subfield
+        self.assertTrue(
+            needs_bookplate_update(old_field, spac_code, spac_name, spac_url)
+        )
+
+    def test_needs_bookplate_update_empty_new_url(self):
+        old_field = Field(
+            tag="966",
+            indicators=[" ", " "],
+            subfields=[
+                Subfield(code="a", value="SPAC"),
+                Subfield(code="b", value="SPAC Name"),
+                Subfield(code="c", value="https://example.com"),
+            ],
+        )
+        spac_code = "SPAC"
+        spac_name = "SPAC Name"
+        spac_url = ""
+
+        # We will need to update the URL, since the new URL is empty
+        self.assertTrue(
+            needs_bookplate_update(old_field, spac_code, spac_name, spac_url)
+        )
+
+    def test_needs_bookplate_update_new_url_mismatch(self):
+        old_field = Field(
+            tag="966",
+            indicators=[" ", " "],
+            subfields=[
+                Subfield(code="a", value="SPAC"),
+                Subfield(code="b", value="SPAC Name"),
+                Subfield(code="c", value="https://example.com"),
+            ],
+        )
+        spac_code = "SPAC"
+        spac_name = "SPAC Name"
+        spac_url = "https://newexample.com"
+
+        # We will need to update the URL, since the new URL doesn't match the original
+        self.assertTrue(
+            needs_bookplate_update(old_field, spac_code, spac_name, spac_url)
+        )
+
+    def test_needs_bookplate_update_new_name_mismatch(self):
+        old_field = Field(
+            tag="966",
+            indicators=[" ", " "],
+            subfields=[
+                Subfield(code="a", value="SPAC"),
+                Subfield(code="b", value="SPAC Name"),
+                Subfield(code="c", value="https://example.com"),
+            ],
+        )
+        spac_code = "SPAC"
+        spac_name = "Different SPAC Name"
+        spac_url = "https://example.com"
+
+        # We will need to update the bookplate text, since the new name doesn't match the original
+        self.assertTrue(
+            needs_bookplate_update(old_field, spac_code, spac_name, spac_url)
+        )
+
+    def test_add_new_966(self):
+        old_record = Record()
+        spac_code = "SPAC"
+        spac_name = "SPAC Name"
+        spac_url = "https://example.com"
+        add_new_966(old_record, spac_code, spac_name, spac_url)
+        self.assertTrue(old_record.get_fields("966"))
+        self.assertEqual(
+            old_record.get_fields("966")[0].get_subfields("a")[0], spac_code
+        )
+        self.assertEqual(
+            old_record.get_fields("966")[0].get_subfields("b")[0], spac_name
+        )
+        self.assertEqual(
+            old_record.get_fields("966")[0].get_subfields("c")[0], spac_url
+        )
+
+    def test_update_existing_966_replace_URL(self):
+        old_field = Field(
+            tag="966",
+            indicators=[" ", " "],
+            subfields=[
+                Subfield(code="a", value="SPAC"),
+                Subfield(code="b", value="SPAC Name"),
+                Subfield(code="c", value="https://example.com"),
+            ],
+        )
+        spac_url = "https://newexample.com"
+        spac_name = "SPAC Name"
+        update_existing_966(old_field, spac_name, spac_url)
+        self.assertEqual(old_field.get_subfields("c")[0], spac_url)
+        self.assertEqual(old_field.get_subfields("b")[0], spac_name)
+
+    def test_update_existing_966_remove_URL(self):
+        old_field = Field(
+            tag="966",
+            indicators=[" ", " "],
+            subfields=[
+                Subfield(code="a", value="SPAC"),
+                Subfield(code="b", value="SPAC Name"),
+                Subfield(code="c", value="https://example.com"),
+            ],
+        )
+        spac_url = ""
+        spac_name = "SPAC Name"
+        update_existing_966(old_field, spac_name, spac_url)
+        self.assertEqual(old_field.get_subfields("c"), [])
+
+
+if __name__ == "__main__":
+    unittest.main()
